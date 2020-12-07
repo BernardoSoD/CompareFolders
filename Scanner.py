dadb9dc530e005a9e62e30b43d82e186997d66a4
Bernardo Duarte <41548476+BernardoSoD@users.noreply.github.com>
2020-12-07 12:05:26 -0300

    Add files via upload

---
+++ Scanner.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+reader = open("saida.txt", "w")
+
+
+def scan(path):
+    with os.scandir(path) as entries:
+        for entry in entries:
+            if entry.is_dir():
+                scan(entry.path)
+            else:
+                reader.write(entry.path[len(initialscan):] + " Size: " + str(os.stat(entry.path).st_size) + "\n")
+
+
+if __name__ == "__main__" and len(sys.argv) > 1:
+    initialscan = sys.argv[1]
+    scan(initialscan)
+else:
+    print("Missing path argument")
